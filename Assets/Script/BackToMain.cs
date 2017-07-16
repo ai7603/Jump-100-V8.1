e59b17e9acb2e0e011a8321a09efd69e4c8813ff
Allen <1693065785@qq.com>
2017-07-16 23:33:25 +0800

    first commit

---
+++ Assets/Script/BackToMain.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class BackToMain : MonoBehaviour {
+    public AudioSource click_audio;
+
+    private bool IsButtonPressed;
+    private int aux_counter;
+    private GameObject SHADE;
+    private GameObject REAL_SHADE;
+    // Use this for initialization
+    void Start () {
+        IsButtonPressed = false;
+        aux_counter = 0;
+
+        SHADE = (GameObject)Resources.Load("Prefab/shadow");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsButtonPressed) {
+            if (aux_counter == 60)
+            {
+                SceneManager.LoadScene("Main");
+            }
+            aux_counter++;
+        }
+	}
+
+	public void backtomain(){
+        click_audio.Play();
+
+        IsButtonPressed = true;
+        REAL_SHADE = Instantiate(SHADE) as GameObject;
+        REAL_SHADE.transform.position = new Vector3(-93.20983f, 68.6339f, 1.0f);
+        REAL_SHADE.transform.localScale = new Vector3(1000.0f, 1000.0f, 1000.0f);
+        // SceneManager.LoadScene ("Main");
+    }
+}
---
+++ Assets/Script/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInfo {
+
+	public static string USER;
+	public static int COIN;
+    public static bool PETS;
+
+    public static int highestscore;
+}
